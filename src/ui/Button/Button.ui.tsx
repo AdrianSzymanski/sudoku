10326cd9fbd820be470d53b21fdacd30f969f900
Adrian Szymański <a.szymanski.1993@gmail.com>
2025-11-02 19:03:16 +0100

    feat(Button): added support for pressed state

--- src/ui/Button/Button.ui.tsx
+++ src/ui/Button/Button.ui.tsx
@@ -1,18 +1,23 @@
+import clsx from 'clsx';
 import './Button.ui.css';
 
 type ButtonProps = React.PropsWithChildren & {
+  isPressed?: boolean;
   isDisabled?: boolean;
   onClick: () => void;
 };
 
 export const Button: React.FC<ButtonProps> = ({
   children,
+  isPressed = false,
   isDisabled = false,
   onClick,
 }) => {
   return (
     <button
-      className='button'
+      className={clsx('button', {
+        'button--is-pressed': isPressed,
+      })}
       type='button'
       disabled={isDisabled}
       onClick={onClick}
