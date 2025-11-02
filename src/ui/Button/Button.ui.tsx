596a70389c3cfd4351003f370503706ca7a2919b
Adrian Szymański <a.szymanski.1993@gmail.com>
2025-11-02 18:24:46 +0100

    feat(Button): added support for disabled state

--- src/ui/Button/Button.ui.tsx
+++ src/ui/Button/Button.ui.tsx
@@ -1,15 +1,21 @@
 import './Button.ui.css';
 
 type ButtonProps = React.PropsWithChildren & {
+  isDisabled?: boolean;
   onClick: () => void;
 };
 
-export const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  isDisabled = false,
+  onClick,
+}) => {
   return (
     <button
       className='button'
-      onClick={onClick}
       type='button'
+      disabled={isDisabled}
+      onClick={onClick}
     >
       <span className='button__content'>
         {children}
