15f4c6a6690b7325893295ac6ad2933c434c7c27
Adrian Szymański <a.szymanski.1993@gmail.com>
2025-11-10 15:17:37 +0100

    feat(Layout): added header slot

--- src/ui/Layout/Layout.ui.tsx
+++ src/ui/Layout/Layout.ui.tsx
@@ -1,18 +1,28 @@
 import './Layout.ui.css';
 
 type LayoutProps = {
+  header: React.ReactNode;
   main: React.ReactNode;
   side: React.ReactNode;
 };
 
-export const Layout: React.FC<LayoutProps> = ({ main, side }) => {
+export const Layout: React.FC<LayoutProps> = ({
+  header,
+  main,
+  side,
+}) => {
   return (
     <div className='layout'>
-      <div className='layout__main'>
-        {main}
+      <div className='layout__header'>
+        {header}
       </div>
-      <div className='layout__side'>
-        {side}
+      <div className='layout__body'>
+        <div className='layout__content'>
+          {main}
+        </div>
+        <div className='layout__side'>
+          {side}
+        </div>
       </div>
     </div>
   );
