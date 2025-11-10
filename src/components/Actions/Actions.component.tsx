94170f94be27a81cb9bb86acafa892ee2ff0f3b9
Adrian Szymański <a.szymanski.1993@gmail.com>
2025-11-10 14:03:07 +0100

    refactor(Actions): put buttons inside NumberPad component

--- src/components/Actions/Actions.component.tsx
+++ src/components/Actions/Actions.component.tsx
@@ -1,6 +1,6 @@
 import { getSudoku } from 'sudoku-gen';
 import { useStore, type PuzzleValueType } from '@store';
-import { Button } from '@ui';
+import { Button, NumberPad } from '@ui';
 import { getFormattedPuzzleData } from './Actions.helper';
 
 type ActionsProps = {
@@ -31,7 +31,7 @@ export const Actions: React.FC<ActionsProps> = ({
 
   return (
     <>
-      <div style={{ display: 'flex', flexFlow: 'row wrap', gap: '16px' }}>
+      <div style={{ display: 'flex', flexFlow: 'row wrap', gap: '16px', marginRight: '32px' }}>
         <Button onClick={handleStartNewPuzzle}>
           New
           </Button>
@@ -48,48 +48,62 @@ export const Actions: React.FC<ActionsProps> = ({
           Redo
         </Button>
       </div>
-      <div style={{ display: 'flex', flexFlow: 'row wrap', gap: '16px' }}>
-        <Button
-          isPressed={valueType === 'normal'}
-          isDisabled={valueType === 'normal'}
-          onClick={() => onValueTypeChange('normal')}
-        >
-          Normal
-        </Button>
-        <Button
-          isPressed={valueType === 'pencil'}
-          isDisabled={valueType === 'pencil'}
-          onClick={() => onValueTypeChange('pencil')}
-        >
-          Pencil
-        </Button>
-        <Button
-          isPressed={valueType === 'candidate'}
-          isDisabled={valueType === 'candidate'}
-          onClick={() => onValueTypeChange('candidate')}
-        >
-          Candidate
-        </Button>
-        <Button
-          isPressed={valueType === 'color'}
-          isDisabled={valueType === 'color'}
-          onClick={() => onValueTypeChange('color')}
-        >
-          Color
-        </Button>
-      </div>
-      <div style={{ display: 'flex', flexFlow: 'row wrap', gap: '16px' }}>
-        {/* @TODO: only call makeMove when there is data difference */}
-        <Button onClick={() => makeMove(selectedCells, valueType, 1)}>1</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 2)}>2</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 3)}>3</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 4)}>4</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 5)}>5</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 6)}>6</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 7)}>7</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 8)}>8</Button>
-        <Button onClick={() => makeMove(selectedCells, valueType, 9)}>9</Button>
-      </div>
+      <NumberPad
+        side={(
+          <>
+            <Button
+              isPressed={valueType === 'normal'}
+              isDisabled={valueType === 'normal'}
+              onClick={() => onValueTypeChange('normal')}
+            >
+              Normal
+            </Button>
+            <Button
+              isPressed={valueType === 'pencil'}
+              isDisabled={valueType === 'pencil'}
+              onClick={() => onValueTypeChange('pencil')}
+            >
+              Pencil
+            </Button>
+            <Button
+              isPressed={valueType === 'candidate'}
+              isDisabled={valueType === 'candidate'}
+              onClick={() => onValueTypeChange('candidate')}
+            >
+              Candidate
+            </Button>
+            <Button
+              isPressed={valueType === 'color'}
+              isDisabled={valueType === 'color'}
+              onClick={() => onValueTypeChange('color')}
+            >
+              Color
+            </Button>
+          </>
+        )}
+        main={(
+          <>
+            {/* @TODO: only call makeMove when there is data difference */}
+            <Button onClick={() => makeMove(selectedCells, valueType, 7)}>7</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 8)}>8</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 9)}>9</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 4)}>4</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 5)}>5</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 6)}>6</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 1)}>1</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 2)}>2</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 3)}>3</Button>
+          </>
+        )}
+        footer={(
+          <Button
+            isFullWidth={true}
+            onClick={() => makeMove(selectedCells, valueType, 0)}
+          >
+            Delete
+          </Button>
+        )}
+      />
     </>
   );
 };
