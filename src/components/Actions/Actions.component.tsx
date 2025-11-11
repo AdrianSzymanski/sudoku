c396e53ee36b227925cff4ac8512d9265e2e484e
Adrian Szymański <a.szymanski.1993@gmail.com>
2025-11-11 15:26:47 +0100

    feat(Actions): replaced button labels with icons

--- src/components/Actions/Actions.component.tsx
+++ src/components/Actions/Actions.component.tsx
@@ -1,5 +1,6 @@
 import { useStore, type PuzzleValueType } from '@store';
-import { Button, NumberPad } from '@ui';
+import { Button, Icon, NumberPad } from '@ui';
+import './Actions.component.css';
 
 type ActionsProps = {
   selectedCells: number[];
@@ -15,60 +16,62 @@ export const Actions: React.FC<ActionsProps> = ({
   const { makeMove } = useStore(state => state);
 
   return (
-    <NumberPad
-      side={(
-        <>
+    <div className='actions'>
+      <NumberPad
+        side={(
+          <>
+            <Button
+              isPressed={valueType === 'normal'}
+              isDisabled={valueType === 'normal'}
+              onClick={() => onValueTypeChange('normal')}
+            >
+              <Icon name='inputTypeNormal' />
+            </Button>
+            <Button
+              isPressed={valueType === 'candidate'}
+              isDisabled={valueType === 'candidate'}
+              onClick={() => onValueTypeChange('candidate')}
+            >
+              <Icon name='inputTypeCandidate' />
+            </Button>
+            <Button
+              isPressed={valueType === 'pencil'}
+              isDisabled={valueType === 'pencil'}
+              onClick={() => onValueTypeChange('pencil')}
+            >
+              <Icon name='inputTypePencilMark' />
+            </Button>
+            <Button
+              isPressed={valueType === 'color'}
+              isDisabled={valueType === 'color'}
+              onClick={() => onValueTypeChange('color')}
+            >
+              <Icon name='inputTypeColor' />
+            </Button>
+          </>
+        )}
+        main={(
+          <>
+            <Button onClick={() => makeMove(selectedCells, valueType, 7)}>7</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 8)}>8</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 9)}>9</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 4)}>4</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 5)}>5</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 6)}>6</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 1)}>1</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 2)}>2</Button>
+            <Button onClick={() => makeMove(selectedCells, valueType, 3)}>3</Button>
+          </>
+        )}
+        footer={(
           <Button
-            isPressed={valueType === 'normal'}
-            isDisabled={valueType === 'normal'}
-            onClick={() => onValueTypeChange('normal')}
+            isFullWidth={true}
+            onClick={() => makeMove(selectedCells, valueType, 0)}
           >
-            Normal
+            Delete
           </Button>
-          <Button
-            isPressed={valueType === 'candidate'}
-            isDisabled={valueType === 'candidate'}
-            onClick={() => onValueTypeChange('candidate')}
-          >
-            Candidate
-          </Button>
-          <Button
-            isPressed={valueType === 'pencil'}
-            isDisabled={valueType === 'pencil'}
-            onClick={() => onValueTypeChange('pencil')}
-          >
-            Pencil
-          </Button>
-          <Button
-            isPressed={valueType === 'color'}
-            isDisabled={valueType === 'color'}
-            onClick={() => onValueTypeChange('color')}
-          >
-            Color
-          </Button>
-        </>
-      )}
-      main={(
-        <>
-          <Button onClick={() => makeMove(selectedCells, valueType, 7)}>7</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 8)}>8</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 9)}>9</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 4)}>4</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 5)}>5</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 6)}>6</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 1)}>1</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 2)}>2</Button>
-          <Button onClick={() => makeMove(selectedCells, valueType, 3)}>3</Button>
-        </>
-      )}
-      footer={(
-        <Button
-          isFullWidth={true}
-          onClick={() => makeMove(selectedCells, valueType, 0)}
-        >
-          Delete
-        </Button>
-      )}
-    />
+        )}
+      />
+    </div>
   );
 };
