b6c902c7f1ec7b8e05e5d523ba2e855661c2e73d
Adrian Szymański <a.szymanski.1993@gmail.com>
2025-11-11 10:02:37 +0100

    refactor(components): moved history actions from Actions to Board

--- src/components/Actions/Actions.component.tsx
+++ src/components/Actions/Actions.component.tsx
@@ -12,93 +12,64 @@ export const Actions: React.FC<ActionsProps> = ({
   valueType,
   onValueTypeChange,
 }) => {
-  const {
-    puzzlePastMoves,
-    puzzleFutureMoves,
-    makeMove,
-    undoMove,
-    redoMove,
-    resetPuzzle,
-  } = useStore(state => state);
+  const { makeMove } = useStore(state => state);
 
   return (
-    <>
-      <div style={{ display: 'flex', flexFlow: 'row wrap', gap: '16px', marginRight: '32px' }}>
-        <Button
-          isDisabled={puzzlePastMoves.length === 0}
-          onClick={undoMove}
-        >
-          Undo
-        </Button>
-        <Button
-          isDisabled={puzzleFutureMoves.length === 0}
-          onClick={redoMove}
-        >
-          Redo
-        </Button>
-        <Button
-          onClick={resetPuzzle}
-          isDisabled={puzzlePastMoves.length === 0 && puzzleFutureMoves.length === 0}
-        >
-          Reset
-        </Button>
-      </div>
-      <NumberPad
-        side={(
-          <>
-            <Button
-              isPressed={valueType === 'normal'}
-              isDisabled={valueType === 'normal'}
-              onClick={() => onValueTypeChange('normal')}
-            >
-              Normal
-            </Button>
-            <Button
-              isPressed={valueType === 'pencil'}
-              isDisabled={valueType === 'pencil'}
-              onClick={() => onValueTypeChange('pencil')}
-            >
-              Pencil
-            </Button>
-            <Button
-              isPressed={valueType === 'candidate'}
-              isDisabled={valueType === 'candidate'}
-              onClick={() => onValueTypeChange('candidate')}
-            >
-              Candidate
-            </Button>
-            <Button
-              isPressed={valueType === 'color'}
-              isDisabled={valueType === 'color'}
-              onClick={() => onValueTypeChange('color')}
-            >
-              Color
-            </Button>
-          </>
-        )}
-        main={(
-          <>
-            {/* @TODO: only call makeMove when there is data difference */}
-            <Button onClick={() => makeMove(selectedCells, valueType, 7)}>7</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 8)}>8</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 9)}>9</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 4)}>4</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 5)}>5</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 6)}>6</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 1)}>1</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 2)}>2</Button>
-            <Button onClick={() => makeMove(selectedCells, valueType, 3)}>3</Button>
-          </>
-        )}
-        footer={(
+    <NumberPad
+      side={(
+        <>
           <Button
-            isFullWidth={true}
-            onClick={() => makeMove(selectedCells, valueType, 0)}
+            isPressed={valueType === 'normal'}
+            isDisabled={valueType === 'normal'}
+            onClick={() => onValueTypeChange('normal')}
           >
-            Delete
+            Normal
           </Button>
-        )}
-      />
-    </>
+          <Button
+            isPressed={valueType === 'candidate'}
+            isDisabled={valueType === 'candidate'}
+            onClick={() => onValueTypeChange('candidate')}
+          >
+            Candidate
+          </Button>
+          <Button
+            isPressed={valueType === 'pencil'}
+            isDisabled={valueType === 'pencil'}
+            onClick={() => onValueTypeChange('pencil')}
+          >
+            Pencil
+          </Button>
+          <Button
+            isPressed={valueType === 'color'}
+            isDisabled={valueType === 'color'}
+            onClick={() => onValueTypeChange('color')}
+          >
+            Color
+          </Button>
+        </>
+      )}
+      main={(
+        <>
+          {/* @TODO: only call makeMove when there is data difference */}
+          <Button onClick={() => makeMove(selectedCells, valueType, 7)}>7</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 8)}>8</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 9)}>9</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 4)}>4</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 5)}>5</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 6)}>6</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 1)}>1</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 2)}>2</Button>
+          <Button onClick={() => makeMove(selectedCells, valueType, 3)}>3</Button>
+        </>
+      )}
+      footer={(
+        <Button
+          isFullWidth={true}
+          onClick={() => makeMove(selectedCells, valueType, 0)}
+        >
+          Delete
+        </Button>
+      )}
+    />
   );
 };
--- src/components/Board/Board.component.tsx
+++ src/components/Board/Board.component.tsx
@@ -1,5 +1,5 @@
 import { useStore } from '@store';
-import { Grid, Cell } from '@ui';
+import { Grid, Cell, Spacer, Button } from '@ui';
 import { getFormattedCells } from './Board.helper';
 
 type BoardProps = {
@@ -13,26 +13,58 @@ export const Board: React.FC<BoardProps> = ({
   onCellClick,
   onCellDoubleClick,
 }) => {
-  const puzzleSetup = useStore(state => state.puzzleSetup);
-  const puzzleCurrentMove = useStore(state => state.puzzleCurrentMove);
+  const {
+    puzzleSetup,
+    puzzleCurrentMove,
+    puzzlePastMoves,
+    puzzleFutureMoves,
+    undoMove,
+    redoMove,
+    resetPuzzle,
+  } = useStore(state => state);
   const cells = getFormattedCells(puzzleSetup, puzzleCurrentMove);
 
   return (
-    <Grid>
-      {/* @TODO: memoize Cell component? */}
-      {cells.map((cell, i) => (
-        <Cell
-          value={cell.value}
-          pencilMarks={cell.pencilMarks}
-          candidates={cell.candidates}
-          colors={cell.colors}
-          isInitial={cell.isInitialValue}
-          isSelected={selectedCells.includes(i)}
-          onClick={() => onCellClick(i)}
-          onDoubleClick={() => onCellDoubleClick(i)}
-          key={i}
-        />
-      ))}
-    </Grid>
+    <Spacer stack='column' align='end'>
+      <Spacer>
+        <Button
+          isDisabled={puzzlePastMoves.length === 0}
+          isFullWidth={true}
+          onClick={undoMove}
+        >
+          Undo
+        </Button>
+        <Button
+          isDisabled={puzzleFutureMoves.length === 0}
+          isFullWidth={true}
+          onClick={redoMove}
+        >
+          Redo
+        </Button>
+        <Button
+          isDisabled={puzzlePastMoves.length === 0 && puzzleFutureMoves.length === 0}
+          isFullWidth={true}
+          onClick={resetPuzzle}
+        >
+          Reset
+        </Button>
+      </Spacer>
+      <Grid>
+        {/* @TODO: memoize Cell component? */}
+        {cells.map((cell, i) => (
+          <Cell
+            value={cell.value}
+            pencilMarks={cell.pencilMarks}
+            candidates={cell.candidates}
+            colors={cell.colors}
+            isInitial={cell.isInitialValue}
+            isSelected={selectedCells.includes(i)}
+            onClick={() => onCellClick(i)}
+            onDoubleClick={() => onCellDoubleClick(i)}
+            key={i}
+          />
+        ))}
+      </Grid>
+    </Spacer>
   );
 };
